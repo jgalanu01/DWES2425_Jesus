3d5eb9c766c71c36f8280c9d4566f539a6294a46
Jesus <jgalanu01@educarex.es>
2024-10-08 10:14:30 +0200

    nuevo

---
+++ U3/04Ejercicio3/tratarCookies.php
@@ -0,0 +1,28 @@
+<?php
+
+if(isset($_POST['guardar1'])){
+    //Comprobar que se han rellando datos
+    if(empty($_POST['nombre'])or empty($_POST['ape'])){
+        //Redirigimos a datos personales
+        header('location:1datosPers.php');
+    }else{
+        //Creamos/modificamos cookie
+
+        setcookie('nombre',$_POST['nombre']);
+        setcookie('ape',$_POST['ape']);
+
+        header('location:2datospago.php');
+
+    }
+
+}elseif(isset($_POST['guardar2'])){
+    //Comprobar que se han rellenado datos
+    if(empty($_POST['tipo'])){
+        header('location:2datosPago.php');
+    }
+else{
+    setcookie('tipo',$_POST['tipo']);
+    header('location:3mostrarDatos.php');
+}
+}
+?>
