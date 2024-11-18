db395e2cd956aff3cdcdee4958123a6c6763fbbf
Jesus <jgalanu01@educarex.es>
2024-11-18 12:41:28 +0100

    Creando proeyctos laravel

---
+++ LARAVEL/prueba/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+   // return view('welcome');
+   return view('holaMundo');
+});
+
+Route::get('/alumnos', function () {
+    return 'Bienvenidos Alumnos';
+   
+ });
+
+ Route::get('/alumnos/{nombre}', function ($nombreA) {
+     return 'Bienvenido '.$nombreA;
+ });
+
+ Route::get('/alumnos/insertar/{nombre}', function ($nombreA) {
+    return 'Página para crear alumno '.$nombreA;
+});
+ 
